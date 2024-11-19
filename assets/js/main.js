571363a5a1241b7b2459855bf7c449819f7af240
joe2024-k <ym68907@gmail.com>
2024-11-19 18:07:24 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+// function getApi() {
+//     var xhttp = XMLHttpRequest;
+//     xhttp.onreadystatechange = function () {
+//         if (this.readystate == 4 && this.status == 200) {
+//             document.querySelector(".demo").innerHTML = this.responseText;
+
+//         }
+//     }
+// }
